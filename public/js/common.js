4c86e5b20b0dd28d629bc6fb67776f783cade740
luckyone <luckyone1221@gmail.com>
2020-04-20 23:26:28 +0300

    Made last page

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,14 @@
 "use strict";
 
-function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
@@ -703,6 +711,38 @@ function eventHandler() {
 		$(this).closest('.order-cont').find('.order-cont__items-list-box').slideToggle(function () {
 			this.classList.toggle('active');
 		});
+	}); //11 comparsion
+
+	var comparsionSlider = new Swiper('.comparsion-swiper-cont', {
+		slidesPerView: 'auto'
+	});
+	$('.prod-item__img-wrap .icon-cross').click(function () {
+		var thisSlide = this.closest('.swiper-slide');
+		var allSlides = thisSlide.parentElement.querySelectorAll('.swiper-slide');
+		var SlideIndex;
+
+		var _iterator7 = _createForOfIteratorHelper(allSlides.entries()),
+				_step7;
+
+		try {
+			for (_iterator7.s(); !(_step7 = _iterator7.n()).done;) {
+				var _step7$value = _slicedToArray(_step7.value, 2),
+						index = _step7$value[0],
+						el = _step7$value[1];
+
+				if (el == thisSlide) {
+					SlideIndex = index;
+				}
+			}
+		} catch (err) {
+			_iterator7.e(err);
+		} finally {
+			_iterator7.f();
+		}
+
+		if (allSlides.length > 2) {
+			comparsionSlider.removeSlide(SlideIndex);
+		}
 	});
 	$("[data-dropdown]").click(function () {
 		$($(this).data("dropdown")).toggleClass('active');
